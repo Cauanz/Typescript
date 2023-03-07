f2ed24b0f9797f066999d13deabcabe3fb55b5cc
Cauan Zelazowski <61758981+Cauanz@users.noreply.github.com>
2023-03-07 18:49:02 -0300

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,5 @@
+"use strict";
+//Tipos: number, string, boolean, any, void, null, undefined, never, object
+let x = 10;
+x = 16;
+console.log(x);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+//Tipos: number, string, boolean, any, void, null, undefined, never, object
+let x:number = 10;
+
+x = 16;
+
+console.log(x);
